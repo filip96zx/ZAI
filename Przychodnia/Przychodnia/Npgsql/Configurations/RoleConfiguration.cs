bfb1c0f38b7db30b8c264adb7f796307b1f4f5dc
Filip Cudny <filip96zx@gmail.com>
2021-04-27 21:12:05 +0200

    addition project

---
+++ Przychodnia/Przychodnia/Npgsql/Configurations/RoleConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Przychodnia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Przychodnia.Npgsql.Configurations
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.HasMany(e => e.UserRoles)
+                .WithOne(e => e.Role)
+                .HasForeignKey(ur => ur.RoleId)
+                .IsRequired();
+
+        }
+    }
+}
